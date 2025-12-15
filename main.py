e92b0936471b0fd3c43d005de10ea3d545a61171
ReisJuliano <d.julianoreis@gmail.com>
2025-12-15 10:13:03 -0300

    Persistir ultimo alarme

--- main.py
+++ main.py
@@ -2,11 +2,20 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+ultimo_alarme = None
+
 
 @app.post("/alarme")
 def criar_alarme(hora: int, minuto: int, label: str = "Alarme"):
-    return {
+    global ultimo_alarme
+    ultimo_alarme = {
         "hora": hora,
         "minuto": minuto,
         "label": label
     }
+    return ultimo_alarme
+
+
+@app.get("/alarme")
+def pegar_alarme():
+    return ultimo_alarme
