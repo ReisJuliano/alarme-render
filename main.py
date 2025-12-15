f66478f38460fe0d926384136a9a3969de48924e
ReisJuliano <d.julianoreis@gmail.com>
2025-12-15 09:50:29 -0300

    Retornar dados do alarme para Automate

--- main.py
+++ main.py
@@ -1,28 +1,12 @@
 from fastapi import FastAPI
-from typing import Optional
 
 app = FastAPI()
 
-# fila simples em memória (suficiente pro free tier)
-fila = []
-
 
 @app.post("/alarme")
-def criar_alarme(
-    hora: int,
-    minuto: int,
-    label: str = "Alarme remoto"
-):
-    fila.append({
+def criar_alarme(hora: int, minuto: int, label: str = "Alarme"):
+    return {
         "hora": hora,
         "minuto": minuto,
         "label": label
-    })
-    return {"status": "ok"}
-
-
-@app.get("/alarme")
-def pegar_alarme():
-    if fila:
-        return fila.pop(0)
-    return {}
+    }
